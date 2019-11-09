2bb263d955264d6a5b4b30df53b40a13843b2619
Mohammed Ataaur Rahaman <ataago7@gmail.com>
2019-11-09 23:49:46 +0530

    Final

---
+++ P3_kolmogorovSmirnov.py
@@ -0,0 +1,41 @@
+# 
+#   P3_kolmogorovSmirnov.py
+#   Kolmogrov Smirnov Test
+#
+#   Created by Mohammed Ataa on 9/11/19.
+#   Copyright © 2019 Ataago. All rights reserved.
+#   
+#   Write a program to show goodness of fit test using Kolmogorov Smirnov Test for the given population of random numbers
+#
+
+# Enter the Random Numbers and store it in sorted format
+Ri = sorted([float(i) for i in input("Enter Random numbers to perform Kolmogorov Smirnov Test: ").split()])
+
+# Total Number of Random Digits
+N = len(Ri)
+
+# Calculating D+ and D- for Each Random Digit
+# 
+#   D+ = [i/N - Ri]
+#   D- = [Ri - (i - 1)/N]
+#       Where i ranges from 1 to N
+Dplus, Dminus = [], []
+
+for i in range(N):   # for i = 0 to N - 1
+    Dplus.append(round((i + 1)/N - Ri[i], 2))
+    Dminus.append(round((Ri[i] - i/N), 2))
+
+Dmax = max(max(Dplus), max(Dminus))
+
+# Printing the Table
+print('\ni\tRi\tD+ = i/N-Ri\tD- = Ri-(i-1)/N\n--------------------------------------------------')
+for i in range(N):
+    print(i + 1, '\t', Ri[i], '\t',Dplus[i], '\t\t', Dminus[i])
+
+print('\nD max : ', Dmax)
+Dcritical = float(input("Enter D Critical Value: "))
+
+if Dmax <= Dcritical:
+    print('Hypothesis H0 is Accepted')
+else:
+    print('Hypothesis H0 is Rejected')
